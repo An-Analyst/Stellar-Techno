81d890a2cabe0d9f148fce8d6c9be71e766917a6
Amol <amolsbhalerao11@gmail.com>
2021-01-12 00:01:31 +0530

    Final Commit Without Session Management

--- app/src/main/java/com/example/amol/stellartechno/Welcome.java
+++ app/src/main/java/com/example/amol/stellartechno/Welcome.java
@@ -2,11 +2,14 @@ package com.example.amol.stellartechno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
     private TextView Welcome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,18 @@ public class Welcome extends AppCompatActivity {
         String email = getIntent().getStringExtra("NAME");
         Welcome = findViewById(R.id.welcome);
         Welcome.setText(email);
+    }
+
+    public void logout(View view) {
+        //this method will remove session and open login screen
+        SessionManagement sessionManagement = new SessionManagement(Welcome.this);
+        sessionManagement.removeSession();
+        moveToLogin();
+    }
+
+    private void moveToLogin() {
+        Intent intent = new Intent(Welcome.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
